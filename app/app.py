ca53d4da9e8588366eeba306274f98a7523bb03f
dolane9 <evan.dolan9@mail.dcu.ie>
2019-10-10 20:15:43 +0100

    Update /user to accept user_id as param

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import json
 import psycopg2
 import secrets as sec
@@ -11,21 +12,35 @@ def home():
     return "online"
 
 
-@app.route('/users')
+@app.route('/user/')
 def get_users():
+    # Get user id supplied
+    user_id = request.args.get('user_id')
+
+    # Create query based on user id supplied in request, if any
+    query = "SELECT * FROM app.user"
+    if user_id is not None:
+        # Update query to return only the user specified
+        query = query + " WHERE user_id = " + user_id
+
     # Setup connection
     conn = psycopg2.connect("dbname={} user={} password={} host={}".format(
         sec.db, sec.user, sec.pw, sec.host))
 
     # Retrieve user data
     cur = conn.cursor()
-    cur.execute("SELECT * FROM app.user")
+    cur.execute(query)
     results = cur.fetchall()
 
     cur.close()
     conn.close()
 
-    return json.dumps(results, indent=4, sort_keys=True, default=str)
+    # return json.
+    return app.response_class(
+        response=json.dumps(results, indent=4, sort_keys=True, default=str),
+        status=200,
+        mimetype='application/json'
+    )
 
 
 if __name__ == '__main__':
